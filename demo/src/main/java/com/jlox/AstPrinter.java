a739799348cdd072d3f3a1d7e0f769055be38d16
Artur Mroz <frost47@gmail.com>
2022-10-03 08:53:40 +0100

    add simple ast printer; parse expressions

---
+++ demo/src/main/java/com/jlox/AstPrinter.java
@@ -0,0 +1,48 @@
+package com.jlox;
+
+import com.jlox.Expr.Binary;
+import com.jlox.Expr.Grouping;
+import com.jlox.Expr.Literal;
+import com.jlox.Expr.Unary;
+
+class AstPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Literal expr) {
+        if (expr.value == null)
+            return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("(").append(name);
+        for (Expr expr : exprs) {
+            sb.append(" ");
+            sb.append(expr.accept(this));
+        }
+        sb.append(")");
+
+        return sb.toString();
+    }
+
+}
