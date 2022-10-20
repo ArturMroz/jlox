0ffbec9a205af43c4254dbb4624932d9687afae9
Artur Mroz <frost47@gmail.com>
2022-10-20 18:08:04 +0100

    add native function 'clock'

--- demo/src/main/java/com/jlox/Interpreter.java
+++ demo/src/main/java/com/jlox/Interpreter.java
@@ -3,18 +3,28 @@ package com.jlox;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.jlox.Expr.Binary;
-import com.jlox.Expr.Grouping;
-import com.jlox.Expr.Literal;
-import com.jlox.Expr.Unary;
-import com.jlox.Expr.Variable;
-import com.jlox.Stmt.Block;
-import com.jlox.Stmt.Expression;
-import com.jlox.Stmt.Print;
-import com.jlox.Stmt.Var;
-
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() {
+                return 0;
+            }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double) System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() {
+                return "<native fn>";
+            }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
