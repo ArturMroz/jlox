185bece43a0a9a9b602b745dad040ad05522e140
Artur Mroz <frost47@gmail.com>
2022-10-26 07:52:11 +0100

    add classes

--- demo/src/main/java/com/jlox/Interpreter.java
+++ demo/src/main/java/com/jlox/Interpreter.java
@@ -207,6 +207,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitClassStmt(Stmt.Class stmt) {
+        environment.define(stmt.name.lexeme, null);
+        LoxClass loxClass = new LoxClass(stmt.name.lexeme);
+        environment.assign(stmt.name, loxClass);
+        return null;
+    }
+
+    @Override
     public Void visitIfStmt(Stmt.If stmt) {
         if (isTruthy(evaluate(stmt.condition))) {
             execute(stmt.thenBranch);
