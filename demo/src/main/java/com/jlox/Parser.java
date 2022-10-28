a788dc5c101f75db0cab2c490797ae20fcab6014
Artur Mroz <frost47@gmail.com>
2022-10-28 08:30:55 +0100

    add properties to classes

--- demo/src/main/java/com/jlox/Parser.java
+++ demo/src/main/java/com/jlox/Parser.java
@@ -164,6 +164,9 @@ class Parser {
             if (expr instanceof Expr.Variable) {
                 Token name = ((Expr.Variable) expr).name;
                 return new Expr.Assign(name, value);
+            } else if (expr instanceof Expr.Get) {
+                Expr.Get get = (Expr.Get) expr;
+                return new Expr.Set(get.object, get.name, value);
             }
 
             // we report the error but we don't _throw_ it as parser isn't in
@@ -331,6 +334,9 @@ class Parser {
         while (true) {
             if (match(LEFT_PAREN)) {
                 expr = finishCall(expr);
+            } else if (match(DOT)) {
+                Token name = consume(IDENTIFIER, "Expect property name after '.'.");
+                expr = new Expr.Get(expr, name);
             } else {
                 break;
             }
